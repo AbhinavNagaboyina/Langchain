909a77d26d86eefa0d2726302f35075b54c84bfc
AbhinavNagaboyina <143286786+AbhinavNagaboyina@users.noreply.github.com>
2023-10-17 16:06:01 -0400

    Add files via upload

--- langchainhelper.py
+++ langchainhelper.py
@@ -7,42 +7,43 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain.callbacks import get_openai_callback
-from langchain.cache import InMemoryCache
 import streamlit as st
+from langchain.memory import ConversationBufferWindowMemory
 
 # OpenAI API secret key
 os.environ["OPENAI_API_KEY"]= openai_key
 
-def code_generator(code_text, language):
+def code_generator(code_text, language, sample_input):
 
     #LLM model
     llm= ChatOpenAI(temperature = 0)
 
     #chat prompt template
-    chat_prompt = PromptTemplate(
-        input_variables=["code_text", "language"],
-        template = "You are a helpful assistant that generates {code_text} code in {language}."
-        )
-
-    langchain.llm_cache = InMemoryCache()
+    chat_prompt1 = PromptTemplate(
+        input_variables=["code_text", "language","sample_input"],
+        template = "You are a helpful assistant that generates {code_text} code in {language} for this given {sample_input}."
+        )  
 
     #log file creation
     logfile = "output.log"
     logger.add(logfile, colorize=True, enqueue=True)
     handler = FileCallbackHandler(logfile)
     
-    chain1 = LLMChain(llm=llm, prompt=chat_prompt, callbacks=[handler], verbose=True, output_key= "code")
+    chain1 = LLMChain(llm=llm, prompt=chat_prompt1, callbacks=[handler], verbose=True, output_key= "code")
 
     with get_openai_callback() as cb:
-        response = chain1({"code_text": code_text, "language": language})
-        logger.info(response)
+        response1 = chain1({"code_text": code_text, "language": language, "sample_input": sample_input})
+        
+        logger.info(response1)
+
         total_tokens = cb.total_tokens
         assert total_tokens > 0
         st.sidebar.write(f"Total Tokens: {cb.total_tokens}")
         st.sidebar.write(f"Prompt Tokens: {cb.prompt_tokens}")
         st.sidebar.write(f"Completion Tokens: {cb.completion_tokens}")
         st.sidebar.write(f"Total Cost (USD): ${cb.total_cost}")
-        return response
+
+        return response1
 
                 
                 
--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import langchainhelper as lch
 st.title("Code Generator UI")
 
 # Text input for code
-code_input = st.text_area("Enter your prompt here:")
+code_input = st.sidebar.text_area("Enter your prompt here:")
 
 # Dropdown for selecting the programming language
 programming_languages = ["Python","Java","C++"]
-selected_language = st.selectbox("Select a programming language:", programming_languages)
+selected_language = st.sidebar.selectbox("Select a programming language:", programming_languages)
+sample_input = st.sidebar.text_input("Enter the sample input here:")
 
 if selected_language == "Python":
     language= "python"
@@ -17,7 +18,7 @@ if selected_language == "Java":
 if selected_language == "C++":
     language= "c++"
 
-if st.button("Generate Code"):
+if st.sidebar.button("Generate Code"):
     if code_input:
-        response = lch.code_generator(code_input,language)
-        st.text(response["code"])
+        response = lch.code_generator(code_input,language,sample_input)
+        st.code(response["code"],language=language)
